cfa63fe2f1840e578088f35e2313e1341b05fe9d
Lukas <lukasduechting@mailbox.org>
2022-06-23 14:30:06 +0200

    finished form

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,7 +1,18 @@
+import {FormEvent, useState} from "react";
 
 export default function MainPage (){
 
+    const [username, setUsername] = useState<string>("")
 
+    const handleUsernameChange = (name: string) => {
+        setUsername(name)
+        console.log(name);
+    }
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        console.log("submit")
+    }
 
     return(
         <div>
@@ -9,9 +20,9 @@ export default function MainPage (){
                 <h1>GitBook</h1>
             </div>
             <div>
-                <form>
-                    <input type = "text"/>
-                    <input type = "submit"/>
+                <form onSubmit={handleSubmit}>
+                    <input type = "text" placeholder={"github username"} value={username} onChange={event => handleUsernameChange(event.target.value)}/>
+                    <input type = "submit" value="Search"/>
                 </form>
             </div>
 
