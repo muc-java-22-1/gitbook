7391fd9fb5f973a26689adbf908bc863df2af800
chris <chrisweber1991@gmail.com>
2022-06-30 17:56:36 +0200

    GitHub Login

--- backend/src/main/java/com/example/demo/GithubAuthController.java
+++ backend/src/main/java/com/example/demo/GithubAuthController.java
@@ -3,23 +3,38 @@ package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.stereotype.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/auth")
 
 public class GithubAuthController {
 
+    private final String clientId;
+    private final String clientSecret;
+    private final RestTemplate restTemplate;
 
+    public GithubAuthController(@Value("${app.oauth.clientId}") String clientId,@Value("${app.oauth.clientSecret}") String clientSecret, RestTemplate restTemplate) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.restTemplate = restTemplate;
+    }
 
     @PostMapping("/githublogin")
-    public GiToken createToken(){
-        return null;
+    public GiToken createToken(@RequestParam String code){
 
+        String url = "https://github.com/login/oauth/access_token?"
+                + "client_id=" + clientId
+                + "&client_secret=" + clientSecret
+                + "&code=" + code;
+
+        return null;
     }
 
 
